ecb0e345fea3ddeb43d0917bbe054f96c5ce4d16
Daniel Moore <polaris@northhorizon.net>
2013-02-17 22:27:30 -0500

    Adding support for simple folder naming in CLI and --extension

--- bin/trap.js
+++ bin/trap.js
@@ -5,7 +5,8 @@
 var fs = require('fs')
   , path = require('path')
   , FileRunner = require('../lib/fileRunner')
-  , configFactory = require('../lib/config');
+  , configFactory = require('../lib/config')
+  , existsSync = fs.existsSync || path.existsSync;
 
 var argv = require('optimist')
   .option('c', {
@@ -13,11 +14,30 @@ var argv = require('optimist')
     usage: 'Path to config file.',
     default: path.join(process.cwd(), 'test', configFactory.defaultConfigFileName)
   })
+  .option('e', {
+    alias: 'extension',
+    usage: 'The extension of test files to look for when using simple paths.',
+    default: '.trap.js',
+    check: function (ext) {
+      if (ext.length < 1 || ext[0] != '.')
+        throw new Error('extension must start with a "."');
+    }
+  })
   .argv;
 
 var config = configFactory.load(argv.config);
 
-var runner = new FileRunner(config, argv._.length > 0 ? argv._ : ['./test/{**,}/*.trap.js']);
+var runner = new FileRunner(
+  config, argv._.length > 0
+    ? argv._.map(resolveTestPath)
+    : ['./test/{**,}/*' + argv.extension ]);
+
 config.attachReporters(runner);
 
 runner.run();
+
+function resolveTestPath(testPath) {
+  return existsSync(testPath) && fs.statSync(testPath).isDirectory()
+    ? path.join(testPath, '{**,}', '*' + argv.extension)
+    : testPath;
+}
